244e16828a4bd7f51813f94ae156e81728ee4d59
duruti <duruti43@gmail.com>
2020-12-05 22:14:45 +0100

    cors

--- game.js
+++ game.js
@@ -1,18 +1,23 @@
 // https://urlmeta.org/docs/
 let name = 'duruti43@gmail.com'
 let key = '1xIpL4kQ489b92GzJuAv'
+// urldont je souhaite recevoir les metadonnées
 let url = 'https://dev.to/ziratsu/rookie-seo-mistakes-35ni'
+
 // Connection Basic Auth
+console.log(btoa(name+':'+key)) // encode 64 
 basicKey = 'ZHVydXRpNDNAZ21haWwuY29tOjF4SXBMNGtRNDg5YjkyR3pKdUF2'
-console.log(btoa(name+':'+key))
+
 function connect(){
     const options = {
         'method' : 'GET',
-        'mode' : 'no-cors',
+   //      'mode' : 'no-cors',
         'headers' : {
-            'authorization' : 'Basic ZHVydXRpNDNAZ21haWwuY29tOjF4SXBMNGtRNDg5YjkyR3pKdUF2',
-            'accept' : 'application/json',
-            'content-Type' : 'application/json'
+           //'Access-Control-Allow-Origin' : 'origine',
+           'authorization' : 'Basic ' + btoa(name+':'+key),
+           //'accept' : 'application/json',
+           //'content-Type' : 'application/json',
+           //'Access-Control-Allow-Credentials': true
         }
     }
     fetch('https://api.urlmeta.org/?url='+url,options)
@@ -20,3 +25,4 @@ function connect(){
     
 }
 connect()
+
